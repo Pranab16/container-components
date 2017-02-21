005086b0b2823baebc54ca08c38ca0cf6940215f
shekhar <shekhar.dokania@sprinklr.com>
2017-02-21 18:29:16 +0530

    Header search bar container component.

--- src/molecules/HeaderSearchBar/HeaderSearchBar.storybook.js
+++ src/molecules/HeaderSearchBar/HeaderSearchBar.storybook.js
@@ -3,9 +3,9 @@ const { storiesOf, action } = require('@kadira/storybook');
 const HeaderSearchBar = require('./HeaderSearchBar');
 
 storiesOf('HeaderSearchBar', module)
-    .add('default', () => (
-        <HeaderSearchBar
-            placeholder={'Which topics are you interested in?'}
-            onSubmit={ action('option value changed') }
-        />
-    ));
+  .add('default', () => (
+    <HeaderSearchBar
+      placeholder={'Which topics are you interested in?'}
+      onSubmit={ action('option value changed') }
+    />
+  ));
--- src/molecules/HeaderSearchBarContainer/HeaderSearchBarContainer.js
+++ src/molecules/HeaderSearchBarContainer/HeaderSearchBarContainer.js
@@ -1,15 +1,16 @@
 const React = require('react');
+const Router = require('react-router').Router;
 const HeaderSearchBar = require('../HeaderSearchBar');
 
 class HeaderSearchBarContainer extends React.Component {
 
-    render() {
-        return (
-            <HeaderSearchBar placeholder={'Which topics are you interested in?'} onSubmit={this.onSubmit}/>
-        );
-    }
+  render() {
+    return (
+      <HeaderSearchBar placeholder={'Which topics are you interested in?'} onSubmit={this.onSubmit}/>
+    );
+  }
 
-    onSubmit = () => {
-
-    }
+  onSubmit = (query) => {
+    Router.transitionTo('/search', {filters: {query: query}});
+  }
 }
